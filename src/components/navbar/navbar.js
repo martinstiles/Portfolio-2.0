b67045a26edd84364920c19276d7baf53ff48093
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2021-08-03 19:25:48 +0200

    add navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,26 +1,46 @@
 import React from 'react'
-import Data from './navbar.data.json'
-import { Link } from 'react-router-dom';
-// import NavbarLink from './navbarLink/navbarLink'
+import Data from '../../data/navbar.data.json'
+import { Link, useLocation } from 'react-router-dom';
 import './navbar.style.scss'
+import DropdownSVG from "../../static/dropdown.svg"
 
 const Navbar = () => {
+    const location = useLocation()
+
+    const getNavbarLinkClass = (route) => {
+        if (route === "/projects/1/" && location.pathname.split("/")[1] === "projects") {
+            return "active-navbar-link"
+        }
+        else {
+            return route === location.pathname ? "active-navbar-link" : "inactive-navbar-link"
+        }
+    }
+
     return (
-        <div className="navbar">
+        <div className="navbar animate__animated animate__fadeInDown">
 
             <h1> {Data["title"].toUpperCase()} </h1>
 
             <ul>
                 {
                     Data["routes"].map(({value, route}) =>
-                        <div className="navbar-link-container">
-                            <div></div>
-                            <Link className="navbar-link" to={route}> {value} </Link>
+                        <div>
+                            <Link className={getNavbarLinkClass(route)} to={route}> {value} </Link>
                         </div>
                     )
                 }
-                {/* <NavbarLink value={value} route={route} /> */}
             </ul>
+
+            <div className="dropdown-container">
+                <button> <img src={DropdownSVG} alt="" /> </button>
+                <div>
+                    {
+                        Data["routes"].map(({value, route}) =>
+                            <Link className="dropdown-link" to={route}> { value } </Link>
+                        )
+                    }
+                </div>
+            </div>
 
         </div>
     )
