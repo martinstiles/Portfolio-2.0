4da65560c5f2b05a3b84e8b169840912da1ff04d
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2021-08-31 20:32:34 +0200

    add and change textual data

--- src/views/skills.js
+++ src/views/skills.js
@@ -4,7 +4,7 @@ import '../style.scss'
 const Skills = () => {
     return (
         <div className="main-container">
-            <h1> Skills </h1>
+            <h1 style={{fontFamily: "'Libre Baskerville', serif", fontStyle: "italic"}}> Coming soon </h1>
         </div>
     )
 }
